e9df41cf3a0625b7f31a8a639bd10d9f50e27dc4
Nevenaa <nevenajovanovic164@gmail.com>
2024-12-07 00:10:34 +0100

    Nekretnina kontroler i zasticene rute

--- nekretnine-be/routes/api.php
+++ nekretnine-be/routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AgentController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route; 
+use App\Http\Controllers\NekretninaController;
 
 
 Route::post('/register', [AuthController::class, 'register']);
@@ -11,3 +12,12 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::get('/agents', [AgentController::class, 'index']);
 Route::get('/agents/{id}', [AgentController::class, 'show']);
+
+
+Route::middleware('auth:sanctum')->group(function () {
+   
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::resource('nekretnine', NekretninaController::class)->only(['index','show', 'store']);
+    Route::get('/pretraga-nekretnina', [NekretninaController::class, 'search']); 
+
+});
