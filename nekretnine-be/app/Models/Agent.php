53efcd88d798ce6d1d65f24b4f552b011daa8530
Bojana Pusonjic <bojanabokapusonjic02@gmail.com>
2024-12-06 20:34:02 +0100

    Osnovni modeli kreirani

---
+++ nekretnine-be/app/Models/Agent.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Agent extends Model
+{
+    use HasFactory;
+
+
+    protected $table = 'agenti';
+
+    protected $fillable = [
+        'imePrezime',
+        'grad',
+        'adresa',
+        'email',
+        'telefon',
+    ];
+}
