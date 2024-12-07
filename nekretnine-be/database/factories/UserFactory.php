514b66aadfdc5f3437c40ccff4c4b10866d3eee6
Nevenaa <nevenajovanovic164@gmail.com>
2024-12-07 16:16:24 +0100

    Sitan lapsus u factory-u

--- nekretnine-be/database/factories/UserFactory.php
+++ nekretnine-be/database/factories/UserFactory.php
@@ -26,7 +26,7 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'password' => Hash::make('buyer'), 
+            'password' => Hash::make('password'), 
             'remember_token' => Str::random(10),
             'role' => fake()->randomElement(['admin', 'buyer']),
         ];
