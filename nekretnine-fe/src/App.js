4839d851c8571b7846a9902d380fc7b2b5d21ed5
Bojana Pusonjic <bojanabokapusonjic02@gmail.com>
2025-02-13 23:01:32 +0100

    Home stranica implementirana

--- nekretnine-fe/src/App.js
+++ nekretnine-fe/src/App.js
@@ -1,21 +1,19 @@
 import './App.css';
+import Footer from './components/footer/Footer';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/navbar/Navbar';
+import Home from './components/home/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Navbar></Navbar>
+        <Routes>
+        <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+      <Footer></Footer>
     </div>
   );
 }
--- nekretnine-fe/src/components/footer/Footer.jsx
+++ nekretnine-fe/src/components/footer/Footer.jsx
@@ -1,21 +1,29 @@
 import React from 'react';
 import './Footer.css';
-import { FaFacebookF, FaInstagram } from 'react-icons/fa';
+import logo from "../../assets/logo.png";
+import { FaFacebookF, FaInstagram, FaPinterestP } from 'react-icons/fa';
+
 const Footer = () => {
   return (
-    <footer className="footer" >
-      <div className="footer-content">
-        <p> PROPERTIA LTD </p>
-        <p> propertiaLTDBusiness@propertia.co </p>
-        <p> +345 8976954</p>
-        <div className="social-links">
-          <p> Visit our socials: </p>
-          <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer"><FaFacebookF /></a>
-          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
+    <footer className="footer">
+      <div className="footer-left">
+        <p>© 2025 PROPERTIA</p>
+      </div>
+
+      <div className="footer-center">
+        <div className="footer-logo">
+          <img src={logo} alt="propertia-logo" className="logo" />
+          <p className="logo-text">PROPERTIA</p>
         </div>
-        
+      </div>
+
+      <div className="footer-right">
+        <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer"><FaFacebookF /></a>
+        <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
+        <a style={{ marginRight: "60px" }} href="https://www.pinterest.com" target="_blank" rel="noopener noreferrer"><FaPinterestP /></a>
       </div>
     </footer>
   );
 };
+
 export default Footer;
